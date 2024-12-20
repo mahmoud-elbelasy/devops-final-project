6a7becdbc05546a1f3a1db4fc463f2b4919105c6
mahmoud ali <88333356+mahmoud-elbelasy@users.noreply.github.com>
2024-12-20 10:12:02 +0200

    api key

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Database file
 DB_FILE = 'weather_data.db'
-API_KEY = 'your_openweathermap_api_key'
+API_KEY = '8fa0e0358820cbec2a80e160ddd524d7'
 
 # Ensure database exists
 def init_db():
